1f5642981d2f0140dffacf92066ec6c2387cda9c
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2025-03-01 02:51:34 +0800

    feat: init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Python development setup with Nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.python311
+            pkgs.python311Packages.fastapi
+            pkgs.python311Packages.uvicorn
+          ];
+
+          shellHook = ''
+            python --version
+          '';
+        };
+      }
+    );
+}
